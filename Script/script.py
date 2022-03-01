5e87e515008b56f06df32c5959f97ee784441cfb
Darsh374 <dp594@njit.edu>
2022-03-01 13:10:29 -0500

    made img greyscale and added comments

--- Script/script.py
+++ Script/script.py
@@ -1,6 +1,17 @@
+#import the required modules
 import cv2 as cv
 
-img = cv.imread('Images/test_image1.jpeg')
+# create img object to be read, second parameter is changing image transparency
+img = cv.imread('Images/test_image1.jpeg', 0)
 
+#allows you to display the image, first parameter 
+# is the name output, second parameter is the img object
 cv.imshow('Case', img)
+
+#wait however many second, this case infinite seconds until user hits a button
+#which will execute the next line of code
 cv.waitKey(0)
+
+#closes all the windows once we press a button, so that we don't have random
+#windows opened
+cv.destroyAllWindows() 
