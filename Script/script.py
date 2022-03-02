23d0be71d5fc0fa99c2a61a6fff5b2b811a2711f
Jeanpierre Rea <rea.jeanpierre@gmail.com>
2022-03-01 23:01:40 -0500

    added template image

--- Script/script.py
+++ Script/script.py
@@ -1,10 +1,13 @@
+import numpy as np
 import cv2
 
 # 0 for grayscale, -1 for color
 img = cv2.imread('Images/test_image1.jpeg', 0)
+template = cv2.imread('Images/blue.jpeg', 0)
+
 
 # resizing image. fx, fy. multiplies the pixels by this number, >1 shrink <1 increases
-img = cv2.resize(img, (0, 0), fx=1.5, fy=1.5)
+img = cv2.resize(img, (0, 0), fx=1, fy=1)
 
 # rotate the image 90 degrees clockwise
 img = cv2.rotate(img, cv2.cv2.ROTATE_90_CLOCKWISE)
