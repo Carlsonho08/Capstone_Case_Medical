f7897817f210023dd679254e52dc289cf7566695
Jeanpierre Rea <rea.jeanpierre@gmail.com>
2022-03-05 13:47:03 -0500

    updated comments, changed values

--- Script/test_pixelmatch.py
+++ Script/test_pixelmatch.py
@@ -1,15 +1,17 @@
 from PIL import Image
 from pixelmatch.contrib.PIL import pixelmatch
 
-# small script to test pixelmatch
+# small script to test pixelmatch, uses python imaging library
 
 # images for the comparison
-img_1 = Image.open("Images/a.jpg")
-img_2 = Image.open("Images/b.jpg")
+img_1 = Image.open("Images/1.jpg")
+img_2 = Image.open("Images/2.jpg")
 
-# comparison image that gets created, mode and with size of first image
+# comparison image that gets created, mode and size of first image
 img_diff = Image.new("RGBA", img_1.size)
 
-mismatch = pixelmatch(img_1, img_2, img_diff, 0.4, includeAA=True)
+# float for threshold sensitivity, includeAA detects and ignores anti-aliased pixels
+mismatch = pixelmatch(img_1, img_2, img_diff, 0.5, includeAA=True)
 
+# save the differences to a new file
 img_diff.save("Images/diff.png")
