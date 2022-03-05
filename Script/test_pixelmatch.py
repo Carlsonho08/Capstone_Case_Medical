9c9c4f9b2bfa71cd4e5e406395c4e048473d9da9
Jeanpierre Rea <rea.jeanpierre@gmail.com>
2022-03-04 22:43:05 -0500

    update pixelmatching script

--- Script/test_pixelmatch.py
+++ Script/test_pixelmatch.py
@@ -4,12 +4,12 @@ from pixelmatch.contrib.PIL import pixelmatch
 # small script to test pixelmatch
 
 # images for the comparison
-img_1 = Image.open("Images/test_image1.jpeg")
-img_2 = Image.open("Images/edited_test_image1.jpeg")
+img_1 = Image.open("Images/a.jpg")
+img_2 = Image.open("Images/b.jpg")
 
 # comparison image that gets created, mode and with size of first image
 img_diff = Image.new("RGBA", img_1.size)
 
-mismatch = pixelmatch(img_1, img_2, img_diff, 0.1, includeAA=True)
+mismatch = pixelmatch(img_1, img_2, img_diff, 0.4, includeAA=True)
 
 img_diff.save("Images/diff.png")
