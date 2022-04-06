b1a72c5e7a5848c1f747b4098749838464b4d3e0
Carlsonho08 <43766498+Carlsonho08@users.noreply.github.com>
2022-04-06 17:05:54 -0400

    debug Actionpage and upload

--- php/action_page.php
+++ php/action_page.php
@@ -1,16 +1,36 @@
 <?php
 if(isset($_POST["formSubmit"]) )
 {
-  echo "Welcome " . $_COOKIE["mysets"] . "<br />";
   
   $varSets = $_POST["mysets"];
-  $image = $_FILES['myfile'];
-  $blueprint = $_FILES['blueprint'];
-  setcookie('blueprint',$blueprint,time()+600);
-  setcookie("mysets", $varSets, time()+600);
-  setcookie("myfile", $image, time()+600);
-echo "$image";
+ // $image = $_FILES['myfile'];
+ // $blueprint = $_FILES['blueprint'];
+ //setcookie('blueprint',$blueprint,time()+600);
+ // setcookie("mysets", $varSets, time()+600);
+ // setcookie("myfile", $image, time()+600);
+  $allowed = array('jpg', 'jpeg', 'png','pdf');
 
+  $fileName = $_FILES['myfile']['name'];
+  $fileTmpName = $_FILES['myfile']['tmp_name'];
+  $fileSize = $_FILES['myfile']['size'];
+  $fileError = $_FILES['myfile']['error'];
+  $fileType = $_FILES['myfile']['type'];
+  
+  $fileExt = explode('.', $fileName);
+  $fileActualExt = strtolower(end($fileExt));
+
+  checkname($fileName,$fileTmpName,$fileSize,$fileActualExt,$allowed);
+
+  $fileName2 = $_FILES['blueprint']['name'];
+  $fileTmpName2 = $_FILES['blueprint']['tmp_name'];
+  $fileSize2 = $_FILES['blueprint']['size'];
+  $fileError2 = $_FILES['blueprint']['error'];
+  $fileType2 = $_FILES['blueprint']['type'];
+
+  $fileExt2 = explode('.', $fileName2);
+  $fileActualExt2 = strtolower(end($fileExt2));
+
+  checkname($fileName2,$fileTmpName2,$fileSize2,$fileActualExt2,$allowed);
 
 
 // execute your Python script from PHP
@@ -24,12 +44,35 @@ echo "$image";
 
 // take response content to embed it into the page
 
-
 //exec("python /path/to/python-script.py");
-}
+} // end of if 
+
+
 else
-{echo"file not uploaded " ;
+{
+  echo"file not uploaded " ;
 }
+
+function checkname($name, $temname, $size,$error,$Actual, $allow)
+{
+  if (in_array($Actual, $allow)) {
+    if ($error === 0) {
+        if ($size < 1000000){
+            $fileNameNew = uniqid('true').".".$Acutal;
+            $fileDestination = 'uploads/'.$fileNameNew;
+            move_uploaded_file($temname, $fileDestination);
+            header ("Location: uploadpage.php?uploadsuccess");
+        } else {
+            echo "Your file is too big!";
+        }
+    }else {
+        echo "There was an error uploading your file!";
+    }
+} else {
+    echo "You must upload files with jpg, jpeg, or png extension";
+}
+}
+
 
 
 ?>
--- php/upload.php
+++ php/upload.php
@@ -4,6 +4,7 @@
 
 <h1>Follow the steps to upload your image for comparsion</h1>
 <p>Select your from the following list</p>
+<form action="/action_page.php" method = "POST" enctype = "multipart/form-data"></form>
   <label for="sets">Choose type of case:</label>
   <select name="mysets" id="mysets">
     <option value="BOS15CMFG">BOS15CMFG</option>
@@ -13,7 +14,6 @@
 
 
 <h3>Show a file-select field which allows only one file to be chosen:</h3>
-<form action="/action_page.php" method = "POST" enctype = "multipart/form-data">
   <label for="myfile">Upload Image taken:</label>
   <input type="file" id="myfile" name="myfile"><br><br>
 
@@ -24,7 +24,7 @@
 
 
 
-    <a href='results.html'>
+    <a href='action_page.php'>
       <button type="button" name="formSubmit" value="submit">Submit</button>
     </a>
 </form>
