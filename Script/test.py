2880aa2381483d8819f117e784017d25cb7978a6
Jeanpierre Rea <rea.jeanpierre@gmail.com>
2022-04-11 18:10:50 -0400

    testing function

---
+++ Script/test.py
@@ -0,0 +1,8 @@
+from test_circlecount import *
+
+# testing function of test_circlecount.py
+
+# test = 'Images/finalsheet.jpg'
+# ask for input in command line for image 
+test = (str)(input('File location: '))
+file = main(test)
