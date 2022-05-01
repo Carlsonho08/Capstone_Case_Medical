e11577590c348616dc28fe509ed84b52ebc35157
Jeanpierre Rea <rea.jeanpierre@gmail.com>
2022-05-01 15:38:02 -0400

    cleaning files

--- Script/script.py
+++ Script/script.py
@@ -1,33 +1,6 @@
 import numpy as np
 import cv2
 import matplotlib.pyplot as plt
-# # load image, 0 for grayscale, -1 for color
-# img = cv2.imread('Images/prefinal sheet.jpg', 0)
-# # load template image to look for in first image
-# template = cv2.imread('Images/finalsheet.jpg', 0)
-# height, width = template.shape
-
-# # different methods for template matching
-# methods = [cv2.TM_CCOEFF, cv2.TM_CCOEFF_NORMED, cv2.TM_CCORR,
-#             cv2.TM_CCORR_NORMED, cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]
-
-# for method in methods:
-#     img2 = img.copy()
-
-#     # performs convolution
-#     result = cv2.matchTemplate(img2, template, method)
-
-#     # returns min max values in the array, min max locations in the array
-#     min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(result)
-#     if method in [cv2.TM_SQDIFF, cv2. TM_SQDIFF_NORMED]:
-#         location = min_loc
-#     else:
-#         location = max_loc
-
-#     # forming the rectangle around the template image
-#     bottom_right = (location[0] + width, location[1] + height)
-#     cv2.rectangle(img2, location, bottom_right, 255, 2)
-
 
 image = cv2.imread('Images/finalsheet.jpg')
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
@@ -45,7 +18,6 @@ plt.imshow(rgb)
 plt.show()
 print("hole count : ", len(cnt))
 
-#cv2.imshow('Template Matching', img2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
     
